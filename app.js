18e67840b788af0081177885d0c50e46525145ea
kafadarova <k.kafadarova@gmail.com>
2018-06-23 22:06:13 +0200

    Add an event listener for all keypresses

---
+++ app.js
@@ -0,0 +1,10 @@
+//create a calculator variable
+const calculator = document.querySelector(‘.calculator’);
+//create a variable for all keys which are children of .calculatorKeys
+const keys = calculator.querySelector(‘.calculatorKeys’);
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+
+  }
+})
