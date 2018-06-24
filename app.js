887ff14b164829797f868da1543b4b3cb9734789
kafadarova <k.kafadarova@gmail.com>
2018-06-24 22:23:17 +0200

    Bugfixes, Add the proper css script to the html file + Add several if statement to check what kind of element triggered the element

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 //create a calculator variable
-const calculator = document.querySelector(‘.calculator’);
+const calculator = document.querySelector('.calculator');
 //create a variable for all keys which are children of .calculatorKeys
-const keys = calculator.querySelector(‘.calculatorKeys’);
+const keys = calculator.querySelector('.calculatorKeys');
 
 keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
+  //The target event (e) property returns the element that triggered the event
+  if (e.target.matches('button')) {}
 
+  //determine the type of key that is clicked
+  //the const key = which key is clicked
+  const key = e.target;
+  //dataset property provides read/write access to all the custom data attributes (data-*) set on the element
+  const action = key.dataset.action;
+
+  //when action variable returns false that means that the key doesnt habe a data-action, which means it is a number key
+  if (!action) {
+    console.log('number key!')
+  }
+
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    console.log('operator key!')
+  }
+
+  if (action === 'decimal') {
+    console.log('decimal key!')
+  }
+
+  if (action === 'clear') {
+    console.log('clear key!')
+  }
+
+  if (action === 'calculate') {
+    console.log('equal key!')
   }
 })
