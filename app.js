4a56eb1563bd6d87841b3d1d017648f583465baf
kafadarova <k.kafadarova@gmail.com>
2018-06-25 18:04:31 +0200

    By hitting the decimal key any number after this hitting should be appended to the the display as well

--- app.js
+++ app.js
@@ -10,50 +10,52 @@ const display = document.querySelector('.calculatorDisplay');
 keys.addEventListener('click', e => {
   //The target event (e) property returns the element that triggered the event
   //the number of the key what was clicked
-  if (e.target.matches('button')) {}
+  if (e.target.matches('button')) {
 
-  //determine the type of key that is clicked
-  //the const key = which key is clicked
-  const key = e.target;
-  //dataset property provides read/write access to all the custom data attributes (data-*) set on the element
-  const action = key.dataset.action;
+    //determine the type of key that is clicked
+    //the const key = which key is clicked
+    const key = e.target;
+    //dataset property provides read/write access to all the custom data attributes (data-*) set on the element
+    const action = key.dataset.action;
 
-  //textContent returns the text content of the element
-  //returns the content of the clicked element
-  const keyContent = key.textContent;
+    //textContent returns the text content of the element
+    //returns the content of the clicked element
+    const keyContent = key.textContent;
 
-  //returns the content of the displayed element
-  const displayedNum = display.textContents;
+    //returns the content of the displayed element
+    const displayedNum = display.textContent;
 
-  //when action variable returns false that means that the key doesnt habe a data-action, which means it is a number key
-  if (!action) {
-    //if the calculator shows 0 - replace it with the clicked key
-    if (displayedNum === '0') {
-      display.textContent = keyContent;
-    } else {
-      //append the clicked key to the displayed number
-      display.textContent = displayedNum + keyContent;
+    //when action variable returns false that means that the key doesnt have a data-action,
+    // which means it is a number key
+    if (!action) {
+      //if the calculator shows 0 - replace it with the clicked key
+      if (displayedNum === '0') {
+        display.textContent = keyContent;
+      } else {
+        //append the clicked key to the displayed number
+        display.textContent = displayedNum + keyContent;
+      }
     }
-  }
 
-  if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    console.log('operator key!')
-  }
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      console.log('operator key!')
+    }
 
-  if (action === 'decimal') {
-    console.log('decimal key!')
-  }
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.';
+    }
 
-  if (action === 'clear') {
-    console.log('clear key!')
-  }
+    if (action === 'clear') {
+      console.log('clear key!')
+    }
 
-  if (action === 'calculate') {
-    console.log('equal key!')
+    if (action === 'calculate') {
+      console.log('equal key!')
+    }
   }
 })
