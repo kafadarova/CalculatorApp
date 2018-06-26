8a7012220055dc57610b65c21ce6d1083c132503
kafadarova <k.kafadarova@gmail.com>
2018-06-26 16:26:56 +0200

    Store the two number and the operator + create a calculate function + display the result

--- app.js
+++ app.js
@@ -25,11 +25,14 @@ keys.addEventListener('click', e => {
     //returns the content of the displayed element
     const displayedNum = display.textContent;
 
+    const previousKeyType = calculator.dataset.previousKeyType;
+
     //when action variable returns false that means that the key doesnt have a data-action,
     // which means it is a number key
     if (!action) {
       //if the calculator shows 0 - replace it with the clicked key
-      if (displayedNum === '0') {
+      //if the previousKeyType is an operator - replace the displayed number with the clicked number
+      if (displayedNum === '0' || previousKeyType === 'operator') {
         display.textContent = keyContent;
       } else {
         //append the clicked key to the displayed number
@@ -46,6 +49,13 @@ keys.addEventListener('click', e => {
       //Using classList is a convenient alternative to accessing an element's list of classes
       //add method adds a specified class
       key.classList.add('is-depressed');
+
+      //add custom attribute
+      calculator.dataset.previousKeyType = 'operator';
+      //the displayed number rigth after hitting the operator
+      calculator.dataset.firstValue = displayedNum;
+      //storing the type of the action, operator which was clicked
+      calculator.dataset.operator = action;
     }
 
     if (action === 'decimal') {
@@ -57,7 +67,12 @@ keys.addEventListener('click', e => {
     }
 
     if (action === 'calculate') {
-      console.log('equal key!');
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      //create a secondValue constant which is equal to the currently displayed number.
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
     //Remove .is-depressed class for all keys
 
@@ -68,3 +83,19 @@ keys.addEventListener('click', e => {
       .forEach(k => k.classList.remove('is-depressed'));
   }
 })
+
+//creating a calculate function
+const calculate = (n1, operator, n2) => {
+  let result = '';
+
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+  return result
+}
