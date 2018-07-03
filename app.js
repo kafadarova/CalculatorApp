46e641b5b4c5d5d43e304ef643b83cbe00876195
kafadarova <k.kafadarova@gmail.com>
2018-07-03 23:20:53 +0200

    Set the calculator to its initial state and clear all cuustom attributes when the textcontent of clear button is equal to AC - that means we have already hit another key before

--- app.js
+++ app.js
@@ -91,8 +91,15 @@ keys.addEventListener('click', e => {
     }
 
     if (action === 'clear') {
+      if (key.textContent === 'AC') {
+        calculator.dataset.firstValue = '';
+        calculator.dataset.modValue = '';
+        calculator.dataset.operator = '';
+        calculator.dataset.previousKeyType = '';
+      } else {
+        key.textContent = 'AC';
+      }
       display.textContent = 0;
-      key.textContent = 'AC';
       //update previousKeyType as a clear for pressed clear key
       calculator.dataset.previousKey = 'clear';
     }
