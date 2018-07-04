34f6edb8dc344ce4165320c0071849941195ac6c
kafadarova <k.kafadarova@gmail.com>
2018-07-04 22:46:36 +0200

    Refactore the code by reducing the reassignments and return the result of the calculation within the statement

--- app.js
+++ app.js
@@ -143,16 +143,13 @@ keys.addEventListener('click', e => {
 
 //creating a calculate function
 const calculate = (n1, operator, n2) => {
-  let result = '';
-
   if (operator === 'add') {
-    result = parseFloat(n1) + parseFloat(n2);
+    return firstNum + parseFloat(n2);
   } else if (operator === 'subtract') {
-    result = parseFloat(n1) - parseFloat(n2);
+    return parseFloat(n1) - parseFloat(n2);
   } else if (operator === 'multiply') {
-    result = parseFloat(n1) * parseFloat(n2);
+    return parseFloat(n1) * parseFloat(n2);
   } else if (operator === 'divide') {
-    result = parseFloat(n1) / parseFloat(n2);
+    return parseFloat(n1) / parseFloat(n2);
   }
-  return result
 }
